6b928eb3bcffba587202e107af055ab818fd3ad4
Jhlechia Qureshi <jhlechiae@gmail.com>
2016-02-18 17:26:43 -0500

    Added relationship

---
+++ app/views/orders/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @order, :id, :amount, :order_date, :user_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :orders
+  resources :users
   get 'static_pages/about'
 
   get 'static_pages/home'
